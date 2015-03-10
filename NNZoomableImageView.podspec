364eb4e7f005a987564a7b184c927c1dd7f010d8
koichi yamamoto <koichi@dividual.jp>
2015-03-10 11:35:31 +0900

    a

--- NNZoomableImageView.podspec
+++ NNZoomableImageView.podspec
@@ -32,4 +32,5 @@ Pod::Spec.new do |s|
   # s.public_header_files = 'Pod/Classes/**/*.h'
   # s.frameworks = 'UIKit', 'MapKit'
   s.dependency "Masonry"
+  s.dependency "NBULog"
 end
--- Pod/Classes/NNZoomableImageView.m
+++ Pod/Classes/NNZoomableImageView.m
@@ -11,7 +11,7 @@
 
 
 @implementation NNZoomableImageView{
-	UIImageView* _iv;
+	UIImageView* _imageView;
 }
 
 
@@ -21,20 +21,77 @@
 	self.minimumZoomScale = 1;
 	self.maximumZoomScale = 3;
 	
-	_iv = [[UIImageView alloc] initWithFrame:CGRectMake(100, 100, 100, 100)];
-	[self addSubview:_iv];
+	_imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 568/2 - 320/2, 320, 320)];
+	[self addSubview:_imageView];
 }
 
+-(void)setFrame:(CGRect)frame{
+	[super setFrame:frame];
+}
+
+
 -(UIView*)viewForZoomingInScrollView:(UIScrollView *)scrollView{
-	return _iv;
+	return _imageView;
+}
+- (void)scrollViewDidZoom:(UIScrollView*)scrollView{
+	// Update image view origin
+	[self _updateImageViewOrigin];
 }
 
 
 -(void)setImage:(UIImage *)image{
 	_image = image;
-	_iv.image = image;
+	_imageView.image = image;
+	[self _updateImageViewSize];
+	[self _updateImageViewOrigin];
 }
 
+
+- (void)_updateImageViewSize{
+	// Get image size
+	CGSize  imageSize;
+	imageSize = _imageView.image.size;
+	
+	// Decide image view size
+	CGRect  bounds;
+	CGRect  rect;
+	bounds = self.bounds;
+	rect.origin = CGPointZero;
+	if (imageSize.width / imageSize.height > CGRectGetWidth(bounds) / CGRectGetHeight(bounds)) {
+		rect.size.width = CGRectGetWidth(bounds);
+		rect.size.height = floor(imageSize.height / imageSize.width * CGRectGetWidth(rect));
+	}
+	else {
+		rect.size.height = CGRectGetHeight(bounds);
+		rect.size.width = imageSize.width / imageSize.height * CGRectGetHeight(rect);
+	}
+	
+	// Set image view frame
+	_imageView.frame = rect;
+}
+
+- (void)_updateImageViewOrigin
+{
+	// Get image view frame
+	CGRect  rect;
+	rect = _imageView.frame;
+	
+	// Get scroll view bounds
+	CGRect  bounds;
+	bounds = self.bounds;
+	
+	// Compare image size and bounds
+	rect.origin = CGPointZero;
+	if (CGRectGetWidth(rect) < CGRectGetWidth(bounds)) {
+		rect.origin.x = floor((CGRectGetWidth(bounds) - CGRectGetWidth(rect)) * 0.5f);
+	}
+	if (CGRectGetHeight(rect) < CGRectGetHeight(bounds)) {
+		rect.origin.y = floor((CGRectGetHeight(bounds) - CGRectGetHeight(rect)) * 0.5f);
+	}
+	
+	// Set image view frame
+	_imageView.frame = rect;
+}
 
 
 
